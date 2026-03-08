fbb072581a93dfae45f76856c823ed214ff9f051
catursetyo <catursetyo26@gmail.com>
2026-03-08 09:54:19 +0700

    update comment in Main.java

--- polymorphism/Noodle.java
+++ polymorphism/Noodle.java
@@ -1,5 +1,21 @@
 package polymorphism;
 
-public class Noodle {
-    
+class Noodle {
+    public void cook() {
+        System.out.println("Memasak mie instan biasa...");
+    }
+}
+
+class Gacoan extends Noodle {
+    @Override
+    public void cook() {
+        System.out.println("Memasak Mie Gacoan level 6...");
+    }
+}
+
+class Indomie extends Noodle {
+    @Override
+    public void cook() {
+        System.out.println("Merebus Indomie al dente...");
+    }
 }
